45ddd54740ce8d7b6c6bbd37bd4f2e6463373bcd
Conor Kelly <ckelly@ckellydesign.net>
2014-09-21 17:51:35 -0700

    Setting model of Index Collection

--- router.js
+++ router.js
@@ -1,10 +1,11 @@
 var expressHandlebars = require('express-hbs');
+var appModel = require('./__app/_models/model.json');
 
 exports.setRoutes = function(app){
 
 	app.get('/', function(req, res){
 		//res.send('hello world');
-		res.render('index', { title: 'Home'});
+		res.render('index', { title: 'Home', model: appModel });
 	});	
 
 };
