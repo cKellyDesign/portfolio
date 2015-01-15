cc06d429e049b6005aa8725d8b18356f58ca3cbc
Conor Kelly <ckelly@ckellydesign.net>
2015-01-14 19:35:51 -0800

    Hooking Project Window up Dynamically

--- __server/_builders/ProjectModelBuilder.js
+++ __server/_builders/ProjectModelBuilder.js
@@ -6,6 +6,6 @@ module.exports.window = function init(projectSlug) {
 	var projectWindowModel = _.filter(ProjectsModel, function(project){
 		return project.slug == projectSlug;
 	});
-	console.log(projectSlug + " Project Window Model: ", projectWindowModel);
+	console.log(projectSlug + " Project Window Model: ", projectWindowModel[0]);
 	return projectWindowModel[0];
 };
