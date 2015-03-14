65b424b404d4133e6c2cf430d1ae680d55a4ac64
Conor Kelly <ckelly@ckellydesign.net>
2015-03-14 12:30:37 -0700

    Creating EventHub for Event Pub Sub Method

--- __app/_scripts/main/require.js
+++ __app/_scripts/main/require.js
@@ -1,5 +1,8 @@
 require([
 	'collections/Index'
 ], function (Index) {
-	window.Index = new Index();
+	window.Index = new Index(); // Init Index
+  CkD = window.CkD || {}; // Create cKellyDesign window object
+  CkD.EventHub = {}; // Create object for event pub / sub
+  _.extend(CkD.EventHub, Backbone.Events); // Extend Backbone Events to EventHub
 });
