28c7389be40cc77a425ef223d04e9475d3f002d5
Conor Kelly <ckelly@ckellydesign.net>
2015-06-20 21:10:15 -0700

    Updating Model to Include More Projects

--- assets/scripts/appScripts.js
+++ assets/scripts/appScripts.js
@@ -2316,12 +2316,283 @@ CkD.loadedModel = {
 					"fullRes": "TemplateFramework-5-MartialArts/MartialArts.html"
 				}
 			]
+		},
+		{
+			"title": "YP Premium Design",
+			"slug": "Premium",
+			"tags": [],
+			"mainImage": {
+				"lowRes": "Premium-Feat1.jpg",
+				"highRes": "",
+				"feature": "Premium-Feat1.jpg"
+			},
+			"description": "Part of my time at YP Holdings I was on the Premium Web Design team, designing and developing custom web sites for small business across the nation.",
+			"bullets": [
+				"UI &amp; UX Design",
+				"Front-End Development",
+				"HTML, CSS, jQuery",
+				"Agency Experience"
+			],
+			"gallery": [
+				{
+					"thumb": "Premium-Thumb1.jpg",
+					"gal": "Premium-Gal1.jpg",
+					"fullRes": "Premium_1_bedfellows/index-new.html"
+				},
+				{
+					"thumb": "Premium-Thumb2.jpg",
+					"gal": "Premium-Gal2.jpg",
+					"fullRes": "Premium_2_Bowman/index-new.html"
+				},
+				{
+					"thumb": "Premium-Thumb3.jpg",
+					"gal": "Premium-Gal3.jpg",
+					"fullRes": "Premium_3_allseasonsonehourair/index-new.html"
+				},
+				{
+					"thumb": "Premium-Thumb4.jpg",
+					"gal": "Premium-Gal4.jpg",
+					"fullRes": "Premium_4_ComfortAir/index-new.html"
+				},
+				{
+					"thumb": "Premium-Thumb5.jpg",
+					"gal": "Premium-Gal5.jpg",
+					"fullRes": "Premium-Gal5a.jpg"
+				}
+			]	
+		},
+		{
+			"title": "Water1st.org",
+			"slug": "Water1stOrg",
+			"tags": [],
+			"mainImage": {
+				"lowRes": "Water1st-Featb.jpg",
+				"highRes": "",
+				"feature": "Water1st-Featb.jpg"
+			},
+			"description": "With Water 1st I remotely designed and developed the front and back ends of a custom Wordpress theme from UX/UI to launch and maintenance. As I worked solo I was given the opportunity to learn anything and everything I needed to overcome any obstacles in delivering this product. Utilizing custom plugins and API integrations, the theme was hand crafted to match the client’s needs.",
+			"bullets": [
+				"UX &amp; UI Design",
+				"HTML5, CSS3, jQuery, PHP5, MySQL",
+				"Custom Wordpress Template"
+			],
+			"gallery": [
+				{
+					"thumb": "Water1st-Thumb1b.jpg",
+					"gal": "Water1st-Gal1b.jpg",
+					"fullRes": "Water1st-1b.jpg"
+				},
+				{
+					"thumb": "Water1st-Thumb2b.jpg",
+					"gal": "Water1st-Gal2b.jpg",
+					"fullRes": "Water1st-2b.jpg"
+				},
+				{
+					"thumb": "Water1st-Thumb3b.jpg",
+					"gal": "Water1st-Gal3b.jpg",
+					"fullRes": "Water1st-3b.jpg"
+				},
+				{
+					"thumb": "Water1st-Thumb4b.jpg",
+					"gal": "Water1st-Gal4b.jpg",
+					"fullRes": "Water1st-4b.jpg"
+				},
+				{
+					"thumb": "Water1st-Thumb5b.jpg",
+					"gal": "Water1st-Gal5b.jpg",
+					"fullRes": "Water1st-5b.jpg"
+				},
+				{
+					"thumb": "Water1st-Thumb6b.jpg",
+					"gal": "Water1st-Gal6b.jpg",
+					"fullRes": "Water1st-6b.jpg"
+				},
+				{
+					"thumb": "Water1st-Thumb7b.jpg",
+					"gal": "Water1st-Gal7b.jpg",
+					"fullRes": "Water1st-7b.jpg"
+				}
+			]	
+		},
+		{
+			"title": "Redbook Online Mobile",
+			"slug": "ROL",
+			"tags": [],
+			"mainImage": {
+				"lowRes": "ROL-Feat.jpg",
+				"highRes": "",
+				"feature": "ROL-Feat.jpg"
+			},
+			"description": "This is the mobile version of the web app Redbook Online. From creating wireframes and IA, to developing user flows, to building and testing prototypes, I was integral to every facet of the process. I collaborated directly with fellow developers through the implementation process to deliver the extensive product under demanding deadlines.",
+			"bullets": [
+				"UX & UI Design",
+				"Mobile App Design",
+				"Data Driven Design",
+				"Wireframes &amp; User-flows"
+			],
+			"gallery": [
+				{
+					"thumb": "ROL-Thumb1.jpg",
+					"gal": "ROL-Gal1.jpg",
+					"fullRes": "ROL-1.jpg"
+				},
+				{
+					"thumb": "ROL-Thumb2.jpg",
+					"gal": "ROL-Gal2.jpg",
+					"fullRes": "ROL-2.jpg"
+				},
+				{
+					"thumb": "ROL-Thumb3.jpg",
+					"gal": "ROL-Gal3.jpg",
+					"fullRes": "ROL-3.jpg"
+				},
+				{
+					"thumb": "ROL-Thumb4.jpg",
+					"gal": "ROL-Gal4.jpg",
+					"fullRes": "ROL-4.jpg"
+				},
+				{
+					"thumb": "ROL-Thumb5.jpg",
+					"gal": "ROL-Gal5.jpg",
+					"fullRes": "ROL-5.jpg"
+				},
+				{
+					"thumb": "ROL-Thumb6.jpg",
+					"gal": "ROL-Gal6.jpg",
+					"fullRes": "ROL-6.jpg"
+				},
+				{
+					"thumb": "ROL-Thumb7.jpg",
+					"gal": "ROL-Gal7.jpg",
+					"fullRes": "ROL-7.jpg"
+				}
+			]	
+		},
+		{
+			"title": "Whitman Redbook Online",
+			"slug": "Redbook",
+			"tags": [],
+			"mainImage": {
+				"lowRes": "Redbook-Feat.jpg",
+				"highRes": "",
+				"feature": "Redbook-Feat.jpg"
+			},
+			"description": "Whitman Publishing has been printing the coin pricing book The Redbook for decades and we designed and developed this web app to present coin data to paying subscribers.",
+			"bullets": [
+				"Web App Design",
+				"UX &amp; UI Design",
+				"Data Centric Design"
+			],
+			"gallery": [
+				{
+					"thumb": "Redbook-Thumb1.jpg",
+					"gal": "Redbook-Gal1.jpg",
+					"fullRes": "Redbook-1.jpg"
+				},
+				{
+					"thumb": "Redbook-Thumb2.jpg",
+					"gal": "Redbook-Gal2.jpg",
+					"fullRes": "Redbook-2.jpg"
+				},
+				{
+					"thumb": "Redbook-Thumb3.jpg",
+					"gal": "Redbook-Gal3.jpg",
+					"fullRes": "Redbook-3.jpg"
+				},
+				{
+					"thumb": "Redbook-Thumb4.jpg",
+					"gal": "Redbook-Gal4.jpg",
+					"fullRes": "Redbook-4.jpg"
+				},
+				{
+					"thumb": "Redbook-Thumb5.jpg",
+					"gal": "Redbook-Gal5.jpg",
+					"fullRes": "Redbook-5.jpg"
+				},
+				{
+					"thumb": "Redbook-Thumb6.jpg",
+					"gal": "Redbook-Gal6.jpg",
+					"fullRes": "Redbook-6.jpg"
+				}
+			]	
+		},
+		{
+			"title": "Whitman Themed Landing Pages",
+			"slug": "WTL",
+			"tags": [],
+			"mainImage": {
+				"lowRes": "WTL-Feat.jpg",
+				"highRes": "",
+				"feature": "WTL-Feat.jpg"
+			},
+			"description": "This was a project to design themed landing pages for Whitman Publishing's new State Series and National Park Series merchandise. The fully interactive map draws in users and provides information on the coin and related state or park before guiding them to the eCommerce portion of the site.",
+			"bullets": [
+				"UI &amp; UX Design",
+				"eCommerce Marketing",
+				"Photoshop, Illustrator, Axure"
+			],
+			"gallery": [
+				{
+					"thumb": "WTL-Thumb1.jpg",
+					"gal": "WTL-Gal1.jpg",
+					"fullRes": "WTL-1.jpg"
+				},
+				{
+					"thumb": "WTL-Thumb2.jpg",
+					"gal": "WTL-Gal2.jpg",
+					"fullRes": "WTL-2.jpg"
+				},
+				{
+					"thumb": "WTL-Thumb3.jpg",
+					"gal": "WTL-Gal3.jpg",
+					"fullRes": "WTL-1.jpg"
+				},
+				{
+					"thumb": "WTL-Thumb4.jpg",
+					"gal": "WTL-Gal4.jpg",
+					"fullRes": "WTL-4.jpg"
+				},
+				{
+					"thumb": "WTL-Thumb5.jpg",
+					"gal": "WTL-Gal5.jpg",
+					"fullRes": "WTL-5.jpg"
+				},
+				{
+					"thumb": "WTL-Thumb6.jpg",
+					"gal": "WTL-Gal6.jpg",
+					"fullRes": "WTL-6.jpg"
+				}
+			]	
 		}
 	],
 	"Contact": {
 		"blurb": "<p>Currently located in Nairobi, Kenya <br>I work both domestically and internationally. I am available for freelance and/or telecommuting opportunities.<p><p class='blue'>Contact me today for your web communication needs!</p>"
 	}
 };
+
+/* Basic Project Data Structure
+
+		{
+			"title": "",
+			"slug": "",
+			"tags": [],
+			"mainImage": {
+				"lowRes": "",
+				"highRes": "",
+				"feature": ""
+			},
+			"description": "",
+			"bullets": [],
+			"gallery": [
+				{
+					"thumb": "",
+					"gal": "",
+					"fullRes": ""
+				}
+			]	
+		}
+
+*/;
 define("../../_models/model", function(){});
 
 /*!
