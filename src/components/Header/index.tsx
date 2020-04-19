85ed1f2a23ae4177fb2628b729b5d1cfce2f0fca
ckellydesign <ckellydesign.net@gmail.com>
2020-04-19 16:54:40 -0700

    Add header links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faFilePdf } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 import './header.scss'
 
 export const Header = () => (
   <header>
     <h1 className="site-header">Conor Kelly</h1>
     <span className="sub-header">Designer / Developer / Leader</span>
-    {/* <nav><ul className="social_links">
-        <li><a href="mailto:ckelly@ckellydesign.net" title="Email" target="_blank"><i class="fa fa-envelope"></i>ckelly  at  ckellydesign  net</a></li>
-        <li><a href="https://github.com/cKellyDesign" title="GitHub" target="_blank"><i class="fa fa-github"></i>ckellydesign</a></li>
-        <li><a href="https://www.linkedin.com/in/ckellydesign/" title="LinkedIn" target="_blank"><i class="fa fa-linkedin"></i>ckellydesign</a></li>
-        <li><a href="http://assets.ckellydesign.net/ConorKelly_Resume.pdf" title="Resume" target="_blank"><i class="fa fa-file-text"></i>Resume</a></li>
-    </ul></nav> */}
+    <nav><ul className="social-links">
+      <li><a href="mailto:ckelly@ckellydesign.net" title="Email" target="_blank">
+        <FontAwesomeIcon icon={faEnvelope} />{`ckelly  at  ckellydesign  net`}
+      </a></li>
+      <li><a href="https://github.com/cKellyDesign" title="GitHub" target="_blank">
+        <FontAwesomeIcon icon={faGithub} />{`ckellydesign`}
+      </a></li>
+      <li><a href="https://www.linkedin.com/in/ckellydesign/" title="LinkedIn" target="_blank">
+        <FontAwesomeIcon icon={faLinkedin} />{`ckellydesign`}
+      </a></li>
+      <li><a href="http://assets.ckellydesign.net/ConorKelly_Resume.pdf" title="Resume" target="_blank">
+        <FontAwesomeIcon icon={faFilePdf} />{`Resume`}
+      </a></li>
+    </ul></nav>
   </header>
 );
