09ebecc7a8da9d2bba76ec60d1a54dce94dc8bf5
ckellydesign <ckellydesign.net@gmail.com>
2020-04-19 18:42:56 -0700

    Fix App Tests

--- src/App/tests/App.test.tsx
+++ src/App/tests/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { mount } from 'enzyme';
 import { render } from '@testing-library/react';
-import App from '../App';
+import { App, history } from '../App';
+import { store } from '../../store'
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = mount(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <App />
+      </ConnectedRouter>
+    </Provider>
+  );
+  
 });
