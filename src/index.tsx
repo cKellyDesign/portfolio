a0366c6c887152adf2cc56c752142d4890d587e7
AccessiT3ch <accessiT3ch@gmail.com>
2024-01-23 14:40:58 -0800

    remove store persistor

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-// import { PersistGate } from "reduxjs-toolkit-persist/integration/react";
 import App from './App/App.tsx'
 import './scss/index.scss'
 import { store } from './store/store.ts'
@@ -9,9 +8,6 @@ import { initPortfolioRouter } from './routes/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    {/* <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate> */}
     <RouterProvider
       router={initPortfolioRouter(<App />)}
       fallbackElement={<h5>Loading...</h5>} />
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,41 +1,12 @@
-
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import {
-  persistReducer,
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "reduxjs-toolkit-persist";
-import { PersistConfig, Persistor } from "reduxjs-toolkit-persist/lib/types";
-import storage from "reduxjs-toolkit-persist/lib/storage";
-// import { sessionSlice, sessionSliceName } from "./session";
 import { portfolioSlice, portfolioSliceName } from "./portfolio";
 
-export const persistConfig: PersistConfig<any> = {
-  key: "conor-kelly-portfolio",
-  storage,
-};
-
 export const rootReducer = combineReducers({
-  // [sessionSliceName]: sessionSlice.reducer,
   [portfolioSliceName]: portfolioSlice.reducer,
 });
 
-export const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    }),
+  reducer: rootReducer,
 });
-
-export const persistor:Persistor = persistStore(store);
 
 export default store;
