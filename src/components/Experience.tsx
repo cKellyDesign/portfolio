2dd0b1861fc4502d1a1fcde5cc083c2986031d6a
AccessiT3ch <accessiT3ch@gmail.com>
2024-01-24 15:23:00 -0800

    Init Experience filters UI

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,14 +1,25 @@
 import { Col, Row } from "react-bootstrap";
-import { useProjectsArray } from "../store/portfolio";
-import { useNavigate } from "react-router-dom";
+import { useFiltersArray, useProjectsArray } from "../store/portfolio";
+import { Link, useNavigate } from "react-router-dom";
 
 export const Experience = () => {
   const experience = useProjectsArray();
+  const filters = useFiltersArray();
   const navigate = useNavigate();
   return (
     <section>
-      <h3 className="text-center text-md-start">My Experience</h3>
-      {/* todo: add filter buttons here */}
+      <h3 className="text-center text-md-start mb-3">My Experience</h3>
+      {/* {filters.length > 0 && (<span>Filter by:</span>)} */}
+      <Row className="justify-content-evenly">
+        {filters.map((filter, index) => (
+          <Col className="mb-4 flex-grow-0 text-nowrap" key={'filter' + index}>
+            <Link
+              to={'/' + filter.slug}
+              className="text-decoration-none"
+            >{filter.title}</Link>
+          </Col>
+        ))}
+      </Row>
       <Row>
         {experience.map((project, index) => (
           <Col
