06aaae092a843920960fb7327b875263f288ab6d
AccessiT3ch <accessiT3ch@gmail.com>
2024-11-13 14:50:42 -0800

    Fix header positioning

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,14 +8,13 @@ export const Header = () => {
         <Col>
           <header className="mb-md-5">
             <h1>Conor Kelly</h1>
-            <LinkStack className="d-md-none" />
+            <LinkStack className="" />
             <h2 className="tagline">
               <span >Creating Accessible Websites to Drive Impact</span>
             </h2>
             <div className="d-md-none mb-5 mt-5">
               <ProfilePic />
             </div>
-            <LinkStack className="d-none d-md-flex" />
             {/* <p className='intro_blurb'>I provide tailored web solutions that elevate digital experiences for organizations, ensuring they are robust, inclusive, and user-friendly.</p> */}
           </header>
         </Col>
